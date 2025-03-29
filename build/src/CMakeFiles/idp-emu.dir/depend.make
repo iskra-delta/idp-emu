62155f2ccda960d1a2e00112d39e5d1177cbe0dc
tstih <tomaz.stih@wischner.co.uk>
2025-03-29 14:26:24 +0000

    Initial commit.

---
+++ build/src/CMakeFiles/idp-emu.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for idp-emu.
+# This may be replaced when dependencies are built.
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#define CHIPS_IMPL
+
+#include <z80.h>
+#include <z80ctc.h>
+#include <z80pio.h>
+
+int main()
+{
+    return 0;
+}
